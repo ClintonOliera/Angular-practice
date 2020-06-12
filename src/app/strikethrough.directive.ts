cb76f78ad807c91e8a042671ea6e4020294d549b
clintonoliera <clintonoliera391@gmail.com>
2020-06-12 18:37:59 +0300

    modified all the components and made the form functional

---
+++ src/app/strikethrough.directive.ts
@@ -0,0 +1,20 @@
+import { Directive , ElementRef, HostListener} from '@angular/core';
+
+@Directive({
+  selector: '[appStrikethrough]'
+})
+export class StrikethroughDirective {
+
+  @HostListener('click') onClicks(){
+this.textDeco('line-through')
+  }
+
+  @HostListener('dblclick') onDoubleClick(){
+    this.textDeco('None');
+  };
+ constructor(private elem:ElementRef){}
+
+ private textDeco(action:string) {
+  this.elem.nativeElement.style.textDecoration=action;
+ }
+};
